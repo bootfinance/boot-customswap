505fa5d2afbb3a454d424adddf62d55cd6fdba59
Eric Newhuis <enewhuis@gmail.com>
2021-09-16 12:13:36 -0500

    ramp A2 up/down/stop tests

--- brownie/tests/swap/test_ramp_A2.py
+++ brownie/tests/swap/test_ramp_A2.py
@@ -10,39 +10,39 @@ ONE_DAY = 24 * 60 * 60
 def test_ramp_A(chain, admin, swap_1, swap_10, swap_100, st_A):
     swap = [swap_1, swap_10, swap_100][st_A]
 
-    initial_A = swap.getA()
+    initial_A2 = swap.getA2()
     future_time = chain.time() + MIN_RAMP_TIME + 5
 
-    tx = swap.rampA(initial_A * MAX_A_CHANGE, future_time, {"from": admin})
+    tx = swap.rampA2(initial_A2 * MAX_A_CHANGE, future_time, {"from": admin})
 
-    assert swap.getA() == initial_A
-    assert swap.swapStorage()['futureA'] == initial_A * MAX_A_CHANGE * A_PRECISION
-    assert swap.swapStorage()['initialATime'] == tx.timestamp
-    assert swap.swapStorage()['futureATime'] == future_time
+    assert swap.getA2() == initial_A2
+    assert swap.swapStorage()['futureA2'] == initial_A2 * MAX_A_CHANGE * A_PRECISION
+    assert swap.swapStorage()['initialA2Time'] == tx.timestamp
+    assert swap.swapStorage()['futureA2Time'] == future_time
 
 
 @given(st_A=strategy("uint", min_value=0, max_value=2))
 def test_ramp_A_final(chain, admin, swap_1, swap_10, swap_100, st_A):
     swap = [swap_1, swap_10, swap_100][st_A]
 
-    initial_A = swap.getA()
+    initial_A2 = swap.getA2()
     future_time = chain.time() + MIN_RAMP_TIME + 5
 
-    swap.rampA(initial_A * MAX_A_CHANGE, future_time, {"from": admin})
+    swap.rampA2(initial_A2 * MAX_A_CHANGE, future_time, {"from": admin})
 
     chain.sleep(MIN_RAMP_TIME + 5)
     chain.mine()
 
-    assert swap.getA() == initial_A * MAX_A_CHANGE
+    assert swap.getA2() == initial_A2 * MAX_A_CHANGE
 
 
 @given(st_A=strategy("uint", min_value=0, max_value=1))
 def test_ramp_A_value_up(chain, admin, swap_1, swap_100, st_A):
     swap = [swap_1, swap_100][st_A]
 
-    initial_A = swap.getA()
+    initial_A2 = swap.getA2()
     future_time = chain.time() + MIN_RAMP_TIME
-    tx = swap.rampA(initial_A * MAX_A_CHANGE, future_time, {"from": admin})
+    tx = swap.rampA2(initial_A2 * MAX_A_CHANGE, future_time, {"from": admin})
 
     initial_time = tx.timestamp
     duration = future_time - tx.timestamp
@@ -50,8 +50,8 @@ def test_ramp_A_value_up(chain, admin, swap_1, swap_100, st_A):
     while chain.time() < future_time:
         chain.sleep(MIN_RAMP_TIME // 10)
         chain.mine()
-        expected = int(initial_A + ((chain.time() - initial_time) / duration) * initial_A)
-        assert 0.999 < expected / swap.getA() <= 1
+        expected = int(initial_A2 + ((chain.time() - initial_time) / duration) * initial_A2)
+        assert 0.999 < expected / swap.getA2() <= 1
 
     chain.sleep(5) # TBD: This seems necessary to clean up otherwise next case fails with not enuf time
     chain.mine()
@@ -61,10 +61,10 @@ def test_ramp_A_value_up(chain, admin, swap_1, swap_100, st_A):
 def test_ramp_A_value_down(chain, admin, swap, swap_100, swap_1000, st_A):
     swap = [swap_100, swap_1000][st_A]
 
-    initial_A = swap.getA()
-    final_A = initial_A // MAX_A_CHANGE
+    initial_A2 = swap.getA2()
+    final_A2 = initial_A2 // MAX_A_CHANGE
     future_time = chain.time() + MIN_RAMP_TIME
-    tx = swap.rampA(final_A, future_time, {"from": admin})
+    tx = swap.rampA2(final_A2, future_time, {"from": admin})
 
     initial_time = tx.timestamp
     duration = future_time - tx.timestamp
@@ -72,47 +72,47 @@ def test_ramp_A_value_down(chain, admin, swap, swap_100, swap_1000, st_A):
     while chain.time() < future_time:
         chain.sleep(MIN_RAMP_TIME // 10)
         chain.mine()
-        expected = int(initial_A - ((chain.time() - initial_time) / duration) * (initial_A - final_A))
+        expected = int(initial_A2 - ((chain.time() - initial_time) / duration) * (initial_A2 - final_A2))
         if expected == 0:
-            assert swap.getA() == final_A
+            assert swap.getA2() == final_A2
         else:
-            assert 0.9 < swap.getA() / expected <= 1
+            assert 0.9 < swap.getA2() / expected <= 1
 
 
 @given(st_A=strategy("uint", min_value=0, max_value=1))
 def test_stop_ramp_A(chain, admin, swap_1, swap_100, st_A):
     swap = [swap_1, swap_100][st_A]
 
-    initial_A = swap.getA()
+    initial_A2 = swap.getA2()
     future_time = chain.time() + MIN_RAMP_TIME
-    swap.rampA(initial_A * MAX_A_CHANGE, future_time, {"from": admin})
+    swap.rampA2(initial_A2 * MAX_A_CHANGE, future_time, {"from": admin})
 
     chain.sleep(ONE_DAY)
 
-    tx_stop = swap.stopRampA({"from": admin})
+    tx_stop = swap.stopRampA2({"from": admin})
 
-    current_A = swap.getA()
+    current_A2 = swap.getA2()
 
     chain.sleep(ONE_DAY)
     chain.sleep(ONE_DAY)
     chain.sleep(ONE_DAY)
 
-    assert swap.getA() == current_A
-    assert swap.swapStorage()['futureA'] // A_PRECISION == current_A
-    assert swap.swapStorage()['initialATime'] == tx_stop.timestamp
-    assert swap.swapStorage()['futureATime'] == tx_stop.timestamp
+    assert swap.getA2() == current_A2
+    assert swap.swapStorage()['futureA2'] // A_PRECISION == current_A2
+    assert swap.swapStorage()['initialA2Time'] == tx_stop.timestamp
+    assert swap.swapStorage()['futureA2Time'] == tx_stop.timestamp
 
 
-def test_ramp_A_only_owner(chain, bob, swap):
+def test_ramp_A2_only_owner(chain, bob, swap):
     with brownie.reverts("Ownable: caller is not the owner"):
-        swap.rampA(0, chain.time() + MIN_RAMP_TIME, {"from": bob})
+        swap.rampA2(0, chain.time() + MIN_RAMP_TIME, {"from": bob})
 
 
-def test_ramp_A_insufficient_time(chain, alice, swap):
+def test_ramp_A2_insufficient_time(chain, alice, swap):
     with brownie.reverts("Ownable: caller is not the owner"):
-        swap.rampA(0, chain.time() + MIN_RAMP_TIME - 1, {"from": alice})
+        swap.rampA2(0, chain.time() + MIN_RAMP_TIME - 1, {"from": alice})
 
 
-def test_stop_ramp_A_only_owner(chain, bob, swap):
+def test_stop_ramp_A2_only_owner(chain, bob, swap):
     with brownie.reverts("Ownable: caller is not the owner"):
-        swap.stopRampA({"from": bob})
+        swap.stopRampA2({"from": bob})
